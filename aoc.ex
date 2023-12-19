c326477b596f33e0892269aa46c4faac520da099
CIOSAI <jyinteractive.tw@gmail.com>
2023-12-19 23:40:23 +0800

    day 2 complete

--- aoc.ex
+++ aoc.ex
@@ -44,3 +44,125 @@ defmodule Aoc.Day1 do
     end)
   end
 end
+
+defmodule Aoc.Day2 do
+  def first_part(input) do
+    subject_to = %{"red" => 12, "green" => 13, "blue" => 14}
+
+    get_game_index = fn str ->
+      str
+      |> String.replace("Game ", "")
+      |> Integer.parse
+      |> elem(0)
+    end
+    entries_extract = fn str ->
+      str
+      |> String.split(",")
+      |> Enum.map(fn column ->
+        column
+        |> String.split
+        |> List.to_tuple
+      end)
+      |> Enum.map(fn column ->
+        {
+          elem(column, 1),
+          elem(column, 0)
+          |> Integer.parse
+          |> elem(0)
+        }
+      end)
+      |> Map.new
+    end
+    game_tuple_extract = fn entry ->
+      {
+        elem(entry, 0) |> get_game_index.(),
+        elem(entry, 1)
+        |> String.split(";")
+        |> Enum.map(fn reveal -> entries_extract.(reveal) end)
+      }
+    end
+    fit_constraints = fn reveal_list ->
+      Enum.all?(reveal_list, fn m ->
+        Enum.all?(subject_to, fn {k, v} ->
+          Map.get(m, k, 0) <= v
+        end)
+      end)
+    end
+    game_to_val = fn tup ->
+      if fit_constraints.(elem(tup, 1)) do
+        elem(tup, 0)
+      else
+        0
+      end
+    end
+
+    input
+    |> String.split("\n")
+    |> List.pop_at(-1)
+    |> elem(1)
+    |> Enum.map(fn i ->
+      i
+      |> String.split(":")
+      |> List.to_tuple
+      |> game_tuple_extract.()
+      |> game_to_val.()
+    end)
+    |> Enum.sum
+  end
+
+  def second_part(input) do
+    entries_extract = fn str ->
+      str
+      |> String.split(",")
+      |> Enum.map(fn column ->
+        column
+        |> String.split
+        |> List.to_tuple
+      end)
+      |> Enum.map(fn column ->
+        {
+          elem(column, 1),
+          elem(column, 0)
+          |> Integer.parse
+          |> elem(0)
+        }
+      end)
+      |> Map.new
+    end
+    game_tuple_extract = fn entry ->
+      {
+        elem(entry, 0),
+        elem(entry, 1)
+        |> String.split(";")
+        |> Enum.map(fn reveal -> entries_extract.(reveal) end)
+      }
+    end
+
+    # addition
+    max_of_color = fn list_of_maps, color ->
+      list_of_maps
+      |> Enum.map(fn j -> Map.get(j, color, 0) end)
+      |> Enum.max
+    end
+    # addition
+
+    input
+    |> String.split("\n")
+    |> List.pop_at(-1)
+    |> elem(1)
+    |> Enum.map(fn i ->
+      i
+      |> String.split(":")
+      |> List.to_tuple
+      |> game_tuple_extract.()
+      # no game_to_val.() here
+    end)
+    # addition
+    |> Enum.map(fn i -> elem(i, 1) end)
+    |> Enum.map(fn i ->
+      max_of_color.(i, "red") * max_of_color.(i, "green") * max_of_color.(i, "blue")
+    end)
+    # addition
+    |> Enum.sum
+  end
+end
